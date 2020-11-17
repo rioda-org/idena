c395e880cec8507bf7b59d9abc876acddf8d2da7
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-11-17 01:48:18 +0100

    Add files via upload

---
+++ shared-node-lan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get install unzip curl screen -y
+
+mkdir datadir && cd datadir
+mkdir idenachain.db && cd idenachain.db
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip && rm idenachain.db.zip
+
+curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest \
+| grep browser_download_url \
+| grep idena-node-linux-0.* \
+| cut -d '"' -f 4 \
+| wget -qi -
+mv idena-* idena-go && chmod +x idena-go
+bash -c 'echo "{\"IpfsConf\":{\"Profile\": \"server\" ,\"FlipPinThreshold\":1},\"Sync\": {\"LoadAllFlips\": true}}" > config.json'
+screen -dmS idena ./idena-go --config=config.json --apikey=123
