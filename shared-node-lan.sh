957d456d458c0bdbe970388735ba17afec6bf511
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-11-20 14:32:01 +0100

    Update shared-node-lan.sh

--- shared-node-lan.sh
+++ shared-node-lan.sh
@@ -13,6 +13,7 @@ curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest \
 | grep idena-node-linux-0.* \
 | cut -d '"' -f 4 \
 | wget -qi -
+
 mv idena-* idena-go && chmod +x idena-go
 bash -c 'echo "{\"IpfsConf\":{\"Profile\": \"server\" ,\"FlipPinThreshold\":1},\"Sync\": {\"LoadAllFlips\": true}}" > config.json'
 screen -dmS idena ./idena-go --config=config.json --apikey=123
