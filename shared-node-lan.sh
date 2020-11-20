2c9df339e531f4f6c1dff6bfeeaae9d0b768aa15
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-11-20 15:19:38 +0100

    Update shared-node-lan.sh

--- shared-node-lan.sh
+++ shared-node-lan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt-get install unzip curl screen openssh-server -y
-
 mkdir datadir
 cd datadir
 mkdir idenachain.db
@@ -10,13 +9,7 @@ unzip idenachain.db.zip
 rm idenachain.db.zip
 cd ..
 cd ..
-
-curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest \
-| grep browser_download_url \
-| grep idena-node-linux-0.* \
-| cut -d '"' -f 4 \
-| wget -qi -
-
+curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
 mv idena-* idena-go && chmod +x idena-go
 bash -c 'echo "{\"IpfsConf\":{\"Profile\": \"server\" ,\"FlipPinThreshold\":1},\"Sync\": {\"LoadAllFlips\": true}}" > config.json'
 screen -dmS idena ./idena-go --config=config.json --apikey=123
