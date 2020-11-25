ff71e92896b579a5f7493b26354e16755715dc4f
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-11-25 16:04:43 +0100

    Add files via upload

---
+++ fix_db_idena_manager.sh
@@ -0,0 +1,10 @@
+idena-manager disable
+cd datadir-node1
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+apt-get install unzip
+mkdir idenachain.db
+cd idenachain.db
+apt-get install unzip
+idena-manager status
