445394a6aa0a539f06036055f33000a6050bc8ba
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-11-29 15:00:16 +0100

    Add files via upload

---
+++ manual_update_node_idena_manager.sh
@@ -0,0 +1,5 @@
+idena-manager disable
+curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
+mv idena-node-linux* idena-node-linux-latest
+chmod +x idena-node-linux-latest
+idena-manager enable
