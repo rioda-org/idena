9c241a4b5c8e2b7d1e419f7401e8b3908cfd3de0
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-11-30 03:43:01 +0100

    Add files via upload

---
+++ fix_db.sh
@@ -0,0 +1,12 @@
+killall screen
+cd datadir
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+apt-get install unzip
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip
+rm idenachain.db.zip
+cd ..
+cd ..
+screen -dmS idena ./idena-go
