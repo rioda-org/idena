01356c5e4cadd72b01c96f0aa97f34cb958ad05b
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-12-08 00:37:56 +0100

    change install path
    let the script install things in dir where the script is run and not be presumed/fixed to /root

--- shared-node.sh
+++ shared-node.sh
@@ -2,12 +2,12 @@
 apt-get update && apt-get upgrade -y
 apt-get install git npm unzip curl screen -y
 
-cd /root
 mkdir datadir && cd datadir
 mkdir idenachain.db && cd idenachain.db
 wget "https://idena.site/idenachain.db.zip"
 unzip idenachain.db.zip && rm idenachain.db.zip
-cd /root
+cd ..
+cd ..
 
 curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest \
 | grep browser_download_url \
@@ -29,8 +29,7 @@ cd idena-node-proxy
 bash -c 'echo "AVAILABLE_KEYS=[\"api1\",\"api2\"]
 IDENA_URL=\"http://localhost:9009\"
 IDENA_KEY=\"'$apikey'\"
-PORT=80
-LOGS_OUTPUT=\"/root/proxy.log\"' > .env
+PORT=80' > .env
 
 npm install
 npm start
