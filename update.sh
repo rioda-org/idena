87a2169be4c2911a72c69f84a6c5d6f36589896f
rioda-org <37978870+rioda-org@users.noreply.github.com>
2020-12-20 02:42:16 +0100

    add fast sync

--- update.sh
+++ update.sh
@@ -1,8 +1,15 @@
+sudo apt-get install unzip -y
 killall screen
 rm /home/idena-*
 rm -r /home/datadir/idenachain.db
 rm -r /home/datadir/ipfs
 cd /home
+mkdir datadir && cd datadir
+mkdir idenachain.db && cd idenachain.db
+wget "https://idena.site/idenachain.db.zip"
+unzip idenachain.db.zip && rm idenachain.db.zip
+cd ..
+cd ..
 curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
 mv idena-node-linux* idena-go
 chmod +x idena-go
