8cf857dbc76ffecef9d8d66e27401e03fd8c655c
rioda-org <37978870+rioda-org@users.noreply.github.com>
2021-02-07 01:28:38 +0100

    Update fix_db_idena_manager.sh

--- fix_db_idena_manager.sh
+++ fix_db_idena_manager.sh
@@ -5,7 +5,7 @@ mkdir idenachain.db
 cd idenachain.db
 sudo apt-get install unzip
 #wget "https://idena.site/idenachain.db.zip"
-wget "https://sync.idena-ar.com/idenachain.db.zip"
+wget "https://sync.idena.site/idenachain.db.zip"
 unzip idenachain.db.zip
 rm idenachain.db.zip
 cd ..
