4058df070456c055f417293e54a339224675576d
rioda-org <37978870+rioda-org@users.noreply.github.com>
2021-09-23 21:41:11 +0200

    Delete shared-node-update.sh

--- shared-node-update.sh
+++
@@ -1,7 +0,0 @@
-killall screen
-rm idena-go
-curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
-mv idena-node-linux* idena-go
-chmod +x idena-go
-screen -dmS node $PWD/start
-echo Update was successfull
