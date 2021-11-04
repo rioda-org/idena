ec217a5a88b281679abf099052dc79f6200f5c81
rioda-org <37978870+rioda-org@users.noreply.github.com>
2021-11-04 01:41:49 +0100

    Delete profile-shared.sh

--- profile-shared.sh
+++
@@ -1,9 +0,0 @@
-killall screen
-rm start
-bash -c 'echo "while :
-do
-./idena-go --config=config.json --profile=shared --apikey=123
-date >> node-restarted.log
-done" > start'
-chmod +x start
-screen -dmS node ./start
