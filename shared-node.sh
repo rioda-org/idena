5b3369753d3d1b091698edac43eb13133622b714
rioda-org <37978870+rioda-org@users.noreply.github.com>
2022-05-28 10:31:49 +0200

    Bump node js to 18.2

--- shared-node.sh
+++ shared-node.sh
@@ -5,12 +5,12 @@ echo "idena-node-proxy already installed"
 else
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git unzip curl screen -y
-# Node.js 16.13 instalation
-wget https://github.com/rioda-org/idena/raw/main/node-v16.13.0-linux-x64.tar.xz
+# Node.js 18.2 instalation
+wget https://nodejs.org/dist/v18.2.0/node-v18.2.0-linux-x64.tar.xz
 sudo mkdir -p /usr/local/lib/nodejs
-sudo tar -xJvf node-v16.13.0-linux-x64.tar.xz -C /usr/local/lib/nodejs
-rm node-v16.13.0-linux-x64.tar.xz
-echo "export PATH=/usr/local/lib/nodejs/node-v16.13.0-linux-x64/bin:$PATH" >> ~/.profile
+sudo tar -xJvf node-v18.2.0-linux-x64.tar.xz -C /usr/local/lib/nodejs
+rm node-v18.2.0-linux-x64.tar.xz
+echo "export PATH=/usr/local/lib/nodejs/node-v18.2.0-linux-x64/bin:$PATH" >> ~/.profile
 . ~/.profile
 
 mkdir datadir && cd datadir
