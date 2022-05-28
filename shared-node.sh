ddab9f5256634269ce4b0e35f71fb2f041de3aa0
rioda-org <37978870+rioda-org@users.noreply.github.com>
2022-05-28 12:10:46 +0200

    Update shared-node.sh

--- shared-node.sh
+++ shared-node.sh
@@ -6,10 +6,10 @@ else
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git unzip curl screen -y
 # Node.js 17.1 instalation
-wget https://nodejs.org/download/release/v17.1.0/node-v17.1.0-linux-x64.tar.gz
+wget https://nodejs.org/download/release/v17.1.0/node-v17.1.0-linux-x64.tar.xz
 sudo mkdir -p /usr/local/lib/nodejs
-sudo tar -xJvf node-v17.1.0-linux-x64.tar.gz -C /usr/local/lib/nodejs
-rm node-v17.1.0-linux-x64.tar.gz
+sudo tar -xJvf node-v17.1.0-linux-x64.tar.xz -C /usr/local/lib/nodejs
+rm node-v17.1.0-linux-x64.tar.xz
 echo "export PATH=/usr/local/lib/nodejs/node-v17.1.0-linux-x64/bin:$PATH" >> ~/.profile
 . ~/.profile
 
