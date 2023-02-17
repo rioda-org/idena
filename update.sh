b4b67a61c45d2b5963ada812463149d3154ead69
rioda-org <37978870+rioda-org@users.noreply.github.com>
2023-02-17 19:29:20 +0100

    Mainnet baby!

--- update.sh
+++ update.sh
@@ -7,11 +7,11 @@ cd /home
 mkdir datadir && cd datadir
 mkdir idenachain.db && cd idenachain.db
 #wget "https://idena.site/idenachain.db.zip"
-wget "https://sync.idena.site/idenachain.db.zip"
+#wget "https://sync.idena.site/idenachain.db.zip"
 unzip idenachain.db.zip && rm idenachain.db.zip
 cd ..
 cd ..
-curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
+curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-1.* | cut -d '"' -f 4 | wget -qi -
 mv idena-node-linux* idena-go
 chmod +x idena-go
 screen -d -m ./idena-go
