0c3c372fe0bdfa731ad95a175397ad0813c7cbc1
rioda-org <37978870+rioda-org@users.noreply.github.com>
2023-02-17 19:31:50 +0100

    Mainnet and ignore fast sync
    Ignore fast sync until unlimited bandwidth storage is figured out for hosting idenachain.db

--- shared-node.sh
+++ shared-node.sh
@@ -11,13 +11,13 @@ sudo apt-get install -y nodejs
 
 mkdir datadir && cd datadir
 mkdir idenachain.db && cd idenachain.db
-wget "https://sync.idena.site/idenachain.db.zip"
-unzip idenachain.db.zip && rm idenachain.db.zip
+#wget "https://sync.idena.site/idenachain.db.zip"
+#unzip idenachain.db.zip && rm idenachain.db.zip
 cd ../..
 
 curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest \
 | grep browser_download_url \
-| grep idena-node-linux-0.* \
+| grep idena-node-linux-1.* \
 | cut -d '"' -f 4 \
 | wget -qi -
 mv idena-* idena-go && chmod +x idena-go
@@ -31,7 +31,7 @@ touch node-restarted.log
 tee update << 'EOF'
 killall screen
 rm idena-go
-curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-0.* | cut -d '"' -f 4 | wget -qi -
+curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep browser_download_url | grep idena-node-linux-1.* | cut -d '"' -f 4 | wget -qi -
 mv idena-node-linux* idena-go
 chmod +x idena-go
 screen -dmS node $PWD/start
